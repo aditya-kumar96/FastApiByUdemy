e8501acd12a0e66b08ee4c76e55fc143064d45df
Aditya Kumar <aadikori9085@gmail.com>
2026-01-11 20:17:29 +0530

    creating admin role

--- routers/auth.py
+++ routers/auth.py
@@ -49,8 +49,8 @@ def authenticate_user(username: str, password: str, db):
 #now we need to validate the user as well as the jwt token which shared by client 
 #so for that , we create getcurrentuser function to validate the jwt token 
 # create access token
-def create_access_token(username: str, user_id: int, expires_delta: timedelta):
-    encode = {"sub": username, "id": user_id}
+def create_access_token(username: str, user_id: int, role:str, expires_delta: timedelta):
+    encode = {"sub": username, "id": user_id , "role":role}
     expires = datetime.now(timezone.utc) + expires_delta
     encode.update({"exp": expires})
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
@@ -61,9 +61,10 @@ async def get_current_user(token:Annotated[str,Depends(oauth2_bearer)]):
         payload = jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
         username : str = payload.get('sub')
         user_id : int = payload.get('id')
+        user_role:str = payload.get('role')
         if username is None or user_id is None :
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail='Could not validate user')
-        return {'username':username , 'id':user_id}
+        return {'username':username , 'id':user_id , 'role':user_role}
     except JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail='Could not validate user')
         
@@ -100,5 +101,5 @@ async def login_for_access_token(form_data: form_data, db: db_dependency):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail='Could not validate user')
-    token = create_access_token(user.username, user.id, timedelta(minutes=20))
+    token = create_access_token(user.username, user.id, user.role   , timedelta(minutes=20))
     return {"access_token": token, "token_type": "Bearer"}
