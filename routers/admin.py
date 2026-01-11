9bf1ac31770365a0998e5d80ab5a500908acd67a
Aditya Kumar <aadikori9085@gmail.com>
2026-01-11 23:52:54 +0530

    delete todo by admin

--- routers/admin.py
+++ routers/admin.py
@@ -24,11 +24,26 @@ db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
 
 
-
-
-@router.get("/todo",status_code=status.HTTP_200_OK)
-async def read_all(user:user_dependency , db:db_dependency ):
-    if user is None or user.get('role') != 'admin' :
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Not a Admin")
+@router.get("/todo", status_code=status.HTTP_200_OK)
+async def read_all(user: user_dependency, db: db_dependency):
+    if user is None or user.get("role") != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Not a Admin"
+        )
     return db.query(Todos).all()
 
+
+@router.delete("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def delete_to_byAdmin(user: user_dependency, db: db_dependency, todo_id: int):
+    if user is None or user.get("role") != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="not a admin"
+        )
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    db.delete(todo_model)
+    db.commit()
+    return {
+        "status":status.HTTP_200_OK,
+        "data":todo_model,
+        "message": "todo deleted"
+        }
