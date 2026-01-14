53f852ef5704968a36e2ae4ab440d4528808b463
Aditya Kumar <aadikori9085@gmail.com>
2026-01-15 02:41:20 +0530

    update the database url to postgresql

--- database.py
+++ database.py
@@ -3,10 +3,10 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Aditya@123'
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Aditya%40123@localhost:5432/TodoApplicationDatabase"
 #need to update the password
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args={'check_same_thread':False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SesssionLocal = sessionmaker(autocommit=False,autoflush=False, bind=engine)
 
 Base = declarative_base()
