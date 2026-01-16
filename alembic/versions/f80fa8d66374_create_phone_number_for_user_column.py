5444da7f0d4b85328caf6c493beeaa4c42284bfb
Aditya Kumar <aadikori9085@gmail.com>
2026-01-16 12:56:33 +0530

    install alembic and upgrade the model with phone number in users , initialize the alembic and upgrade the model

---
+++ alembic/versions/f80fa8d66374_create_phone_number_for_user_column.py
@@ -0,0 +1,28 @@
+"""Create phone number for User column
+
+Revision ID: f80fa8d66374
+Revises: 
+Create Date: 2026-01-16 12:45:19.337821
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'f80fa8d66374'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.add_column('users',sa.Column('phone_number',sa.String(),nullable=True))
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
