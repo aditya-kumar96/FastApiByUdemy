a1966e202f63d715e8ba2101a33f786b857ebdd1
Aditya Kumar <aadikori9085@gmail.com>
2026-01-20 11:55:01 +0530

    create a api to get the health of our app

--- main.py
+++ main.py
@@ -6,7 +6,16 @@ from sqlalchemy.orm import Session
 
 from routers import auth, todos,admin,users
 
+
+
 app = FastAPI()
+
+
+@app.get("/healthy")
+async def health_check():
+    return {
+        'status':"Healthy"
+    }
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
