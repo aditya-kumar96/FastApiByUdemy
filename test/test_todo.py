493d7667523db171559f7883eb6096bdaefbb72f
Aditya Kumar <aadikori9085@gmail.com>
2026-01-20 12:30:57 +0530

    added sessionmaker , and create engine

--- test/test_todo.py
+++ test/test_todo.py
@@ -3,10 +3,21 @@
 #app should be inside TestClient , so pytest will understand that it is the mockup of our production level apis
 from sqlalchemy import create_engine
 from sqlalchemy.pool import StaticPool
+from sqlalchemy.orm import sessionmaker
+from ..database import Base
 #create a new database
 SQLALCHEMY_DATABASE_URL = "sqlite:///./testdb.db"
 
 engine= create_engine(SQLALCHEMY_DATABASE_URL , 
                       connect_args={"check_same_thread":False},
                       poolclass=StaticPool)
+
+#create a SessionLocal
+
+TestingSessionLocal = sessionmaker(autocommit= False , autoflush = False , bind = engine)
+
+
+Base.metadata.create_all(bind=engine)
+
+#now override the getdb
 
