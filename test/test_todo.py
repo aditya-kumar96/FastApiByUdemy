e72de040f9d560fc6336d241da50de0743d18ce9
Aditya Kumar <aadikori9085@gmail.com>
2026-01-21 11:44:17 +0530

    added the line of response.json()

--- test/test_todo.py
+++ test/test_todo.py
@@ -50,3 +50,4 @@ client = TestClient(app)
 def test_read_all_authenticated():
     response = client.get("/todo/")
     assert response.status_code == status.HTTP_200_OK
+    assert response.json() == []
