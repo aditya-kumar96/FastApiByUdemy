333ba49694a86a4708234f00e264f641fe63bbc2
Aditya Kumar <aadikori9085@gmail.com>
2026-01-23 18:36:15 +0530

    create utils file and add all the dependencies which will re-use all the time in the testing

--- test/test_todo.py
+++ test/test_todo.py
@@ -11,66 +11,9 @@ from fastapi.testclient import TestClient
 from fastapi import status
 import pytest
 from models import Todos
+from .utils import *
 
 
-# create a new database
-SQLALCHEMY_DATABASE_URL = "sqlite:///./testdb.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False},
-    poolclass=StaticPool,
-)
-
-# create a SessionLocal
-
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-Base.metadata.create_all(bind=engine)
-
-# now override the getdb
-
-
-def override_get_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-# get the current user
-def override_get_current_user():
-    return {"username": "adityak3", "id": 1}
-
-
-app.dependency_overrides[get_db] = override_get_db
-app.dependency_overrides[get_current_user] = override_get_current_user
-
-
-client = TestClient(app)
-
-# read the todo
-
-
-@pytest.fixture
-def test_todo():
-    todo = Todos(
-        title="this is the first todo",
-        description="this todo creating in postgresql",
-        priority=5,
-        complete=True,
-        owner_id=1,
-    )
-
-    db = TestingSessionLocal()
-    db.add(todo)
-    db.commit()
-    yield todo
-    with engine.connect() as connection:
-        connection.execute(text("DELETE FROM todos;"))
-        connection.commit()
 
 
 # read all todo the authentication of todo
