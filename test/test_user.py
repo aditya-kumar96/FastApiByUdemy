6035f51476b35d2a92d2bed3b66aeddc491b2eda
Aditya Kumar <aadikori9085@gmail.com>
2026-01-23 20:57:37 +0530

    create the test user and now create pytest.fixture to create user and test

--- test/test_user.py
+++ test/test_user.py
@@ -6,8 +6,10 @@ from fastapi import status
 app.dependency_overrides[get_db] = override_get_db
 app.dependency_overrides[get_current_user] = override_get_current_user
 
-
+#get the users
 def test_return_user():
     response = client.get('/users')
     assert response.status_code == status.HTTP_200_OK
     assert response.json() is None
+
+
--- test/utils.py
+++ test/utils.py
@@ -7,7 +7,8 @@ from routers.todos import get_db, get_current_user
 from fastapi.testclient import TestClient
 from fastapi import status
 import pytest
-from models import Todos
+from models import Todos, Users
+from routers.auth import bycrpt_context
 
 
 # create a new database
@@ -39,10 +40,7 @@ def override_get_db():
 
 # get the current user
 def override_get_current_user():
-    return {"username": "adityak3", "id": 1,"role":"admin"}
-
-
-
+    return {"username": "adityak3", "id": 1, "role": "admin"}
 
 
 app.dependency_overrides[get_db] = override_get_db
@@ -70,4 +68,28 @@ def test_todo():
     yield todo
     with engine.connect() as connection:
         connection.execute(text("DELETE FROM todos;"))
+        connection.commit()
+
+
+# just like todo , we create for Users to create and test the user
+
+
+@pytest.fixture
+def test_users():
+    user = Users(
+        email="user@gmail.com",
+        username="user",
+        first_name="abc",
+        last_name="bca",
+        hashed_password=bycrpt_context.hash("testpass"),
+        role="admin",
+        phone_number="8273182382",
+    )
+
+    db = TestingSessionLocal()
+    db.add(user)
+    db.commit()
+    yield user
+    with engine.connect() as connection:
+        connection.execute(text("DELETE from users;"))
         connection.commit()
