9e1eeebbc63f89c04e816cabf873cb7c94d11174
Aditya Kumar <aadikori9085@gmail.com>
2026-01-23 20:59:35 +0530

    now test the user

--- test/test_user.py
+++ test/test_user.py
@@ -1,15 +1,15 @@
 from .utils import *
-from routers.users import get_db,get_current_user
+from routers.users import get_db, get_current_user
 from fastapi import status
 
 
 app.dependency_overrides[get_db] = override_get_db
 app.dependency_overrides[get_current_user] = override_get_current_user
 
-#get the users
-def test_return_user():
-    response = client.get('/users')
+
+# get the users
+def test_return_user(test_users):
+    response = client.get("/users")
     assert response.status_code == status.HTTP_200_OK
-    assert response.json() is None
-
-
+    assert response.json()["username"] == "user"
+    assert response.json()["email"] == "user@gmail.com"
