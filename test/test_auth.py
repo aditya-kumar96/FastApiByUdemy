452366d13d1bdf2de43054d9a5304e7657407c4d
Aditya Kumar <aadikori9085@gmail.com>
2026-01-24 23:42:39 +0530

    got error while authenticate user

--- test/test_auth.py
+++ test/test_auth.py
@@ -10,6 +10,8 @@ app.dependency_overrides[get_current_user] = override_get_current_user
 def test_authenticate_user(test_users):
     db = TestingSessionLocal()
     
-    authenticated_user = authenticated_user(test_users.username,'testpass',db)
-    assert authenticated_user is not None
-    assert authenticated_user.username == test_users.username
+    user = authenticate_user(test_users.username,'testpass',db)
+    assert user is not None
+    assert user.username == test_users.username
+    
+    
