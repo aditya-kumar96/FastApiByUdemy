c2ea353bf5c63c80d1f44ffff4bee598bf9ce803
Aditya Kumar <aadikori9085@gmail.com>
2026-01-25 00:20:57 +0530

    test create access token

--- test/test_auth.py
+++ test/test_auth.py
@@ -1,5 +1,8 @@
 from .utils import *
-from routers.auth import get_current_user,get_db,authenticate_user
+from routers.auth import get_current_user,get_db,authenticate_user,create_access_token,SECRET_KEY,ALGORITHM
+from jose import jwt
+from datetime import timedelta
+
 
 app.dependency_overrides[get_db] = override_get_db
 app.dependency_overrides[get_current_user] = override_get_current_user
@@ -22,5 +25,22 @@ def test_authenticate_user(test_users):
     
     wrong_password_user = authenticate_user(test_users.username,'password',db)
     assert wrong_password_user is False
+    
+    
+#test create access token
+
+def test_create_access_token():
+    username = 'testuser'
+    user_id = 1
+    role = 'user'
+    expires_delta = timedelta(days=1)
+    
+    token = create_access_token(username,user_id,role,expires_delta)
+    
+    decoded_token = jwt.decode(token,SECRET_KEY,algorithms=ALGORITHM,options={'verify_signature':False})
+    
+    assert decoded_token['sub'] == username
+    assert decoded_token['id'] == user_id
+    assert decoded_token['role'] == role
     
     
