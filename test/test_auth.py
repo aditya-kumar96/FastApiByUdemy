4e7b7880ab4bbd97f67431557344fd3e7321d5e5
Aditya Kumar <aadikori9085@gmail.com>
2026-01-24 23:37:46 +0530

    test authenticated user ,

--- test/test_auth.py
+++ test/test_auth.py
@@ -6,3 +6,10 @@ app.dependency_overrides[get_current_user] = override_get_current_user
 
 
 #now test the user is authenticate or not
+
+def test_authenticate_user(test_users):
+    db = TestingSessionLocal()
+    
+    authenticated_user = authenticated_user(test_users.username,'testpass',db)
+    assert authenticated_user is not None
+    assert authenticated_user.username == test_users.username
